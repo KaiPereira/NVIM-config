85e9a4f085f5babb8ee7a884bd1fa40da68c6836
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 14:20:39 -0700

    updating my vim config :D

--- init.lua
+++ init.lua
@@ -215,6 +215,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
